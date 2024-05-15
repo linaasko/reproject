fc144c4ab468bb20919d388fcde64a4a6333b0b4
Lina <kokoshalina@gmail.com>
2024-05-16 02:41:21 +0300

    add workspace as target folder choose file for target projection check the spatial reference for each file in folder project files in target spatial reference

--- Reproject.py
+++ Reproject.py
@@ -2,4 +2,9 @@ import arcpy
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = arcpy.GetParameterAsText(0)
 target_spatial_ref = arcpy.Describe(arcpy.GetParameterAsText(1)).spatialReference
+fcList = arcpy.ListFeatureClasses()
+for fc in fcList:
+    if arcpy.Describe(fc).spatialReference.name != target_spatial_ref.name:
+        arcpy.management.Project(fc, fc[:-4] + "_projected.shp" if fc.endswith(".shp") else fc + "_projected", target_spatial_ref)
+
 
