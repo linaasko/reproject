f90ec6c162470e9a8724ed5d11793eb26dbc83ad
Lina <kokoshalina@gmail.com>
2024-05-16 02:38:45 +0300

    add workspace as target folder choose file for target projection

--- Reproject.py
+++ Reproject.py
@@ -1,4 +1,5 @@
 import arcpy
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = arcpy.GetParameterAsText(0)
+target_spatial_ref = arcpy.Describe(arcpy.GetParameterAsText(1)).spatialReference
 
